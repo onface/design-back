7074e8d35263efaaf8148ea45b4f48df26cb8839
Nimo <nimo.jser@gmail.com>
2017-12-22 16:46:06 +0800

    theme

--- s.js
+++ s.js
@@ -3,5 +3,5 @@ var server = livereload.createServer();
 server.watch(__dirname);
 var express = require('express')
 var app = require('express')()
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/output'))
 app.listen(3000)
